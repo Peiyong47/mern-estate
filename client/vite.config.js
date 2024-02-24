01f03b2692dc8cfff302bb662c874b3901be63dd
Pei Yong <peiyong0890@gmail.com>
2024-02-24 21:17:37 +0800

    create user successfully and save to database

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    // add a proxy to the server to avoid CORS issues
+    // to the api routes
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    }, 
+  },
   plugins: [react()],
 })
