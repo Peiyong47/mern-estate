d1eae7774ee781a32ea78dea3f57d1657610fb63
Pei Yong <peiyong0890@gmail.com>
2024-02-25 23:13:25 +0800

    add redux persist to save data in the local storage of the application

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer =  combineReducers({user: userReducer});
+
+// persistConfig is setting the name of the key in the local storage, the version and the storage type
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => 
     getDefaultMiddleware({
         serializableCheck: false,
     }),
-})
+});
+
+// persistor is used to persist the state of the store
+export const persistor = persistStore(store);
