2accdaff893e6316e59180ccdec1493443441751
Pei Yong <peiyong0890@gmail.com>
2024-02-26 15:16:01 +0800

    complete creating listing page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import About from './pages/About'
 import Home from './pages/Home'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
+import CreateListing from './pages/CreateListing'
 
 export default function App() {
   // private route is to cover the pages that only logged in users can access
@@ -21,6 +22,7 @@ export default function App() {
         
         <Route element={<PrivateRoute />} >
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
