849c1c56387bb93e3303c31cbf9c75b29a9a2c7b
Pei Yong <peiyong0890@gmail.com>
2024-02-27 17:35:19 +0800

    add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -17,7 +17,7 @@ export default function Search() {
 
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);  
-    console.log(listings);   
+    const [showMore, setShowMore] = useState(false);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search); 
@@ -51,10 +51,14 @@ export default function Search() {
 
         const fetchListings = async () => {
             setLoading(true);
+            setShowMore(false);
             try {
                 const searchQuery = urlParams.toString();
                 const res = await fetch(`/api/listing/get?${searchQuery}`);
                 const data = await res.json();
+                if (data.length > 8) {
+                    setShowMore(true);
+                }
                 setListings(data);
                 setLoading(false);
             } catch (error) {
@@ -113,6 +117,23 @@ export default function Search() {
 
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
+    }
+
+    const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+
+        if (data.length > 9){
+            setShowMore(true);
+        } else {
+            setShowMore(false);
+        }
+        setListings([...listings, ...data]);
     }
 
     return (
@@ -229,11 +250,21 @@ export default function Search() {
                 {loading && (
                     <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
                 )}
-               {    !loading &&
+                {!loading &&
                     listings &&
                     listings.map((listing) => (
                     <ListingItem key={listing._id} listing={listing} />
-            ))}
+                ))}
+                {
+                    showMore && (
+                        <button
+                            onClick={onShowMoreClick}
+                            className='text-green-700 hover:underline p-7 text-center w-full'
+                        >
+                            Show More
+                        </button>
+                    )
+                }
             </div>
         </div>
     </div>
